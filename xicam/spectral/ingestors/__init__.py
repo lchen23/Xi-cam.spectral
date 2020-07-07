a6b6f5a80892446718e6e15c50b7abdc604abb3c
Juliane Reinhardt <jreinhardt@lbl.gov>
2020-07-07 00:56:37 -0700

    begin cxi ingestor

--- xicam/spectral/ingestors/__init__.py
+++ xicam/spectral/ingestors/__init__.py
@@ -4,7 +4,7 @@ import event_model
 from pathlib import Path
 import dask.array as da
 from xarray import DataArray
-
+from scipy.constants import h, e, speed_of_light
 
 def ingest_nxSTXM(paths):
 
@@ -26,6 +26,71 @@ def ingest_nxSTXM(paths):
                          'irmap/DATA/energy': energy,
                          'irmap/DATA/sample_x': sample_x,
                          'irmap/DATA/sample_y': sample_y}
+    )]
+
+    # Compose run start
+    run_bundle = event_model.compose_run()  # type: event_model.ComposeRunBundle
+    start_doc = run_bundle.start_doc
+    start_doc["sample_name"] = Path(paths[0]).resolve().stem
+    start_doc["projections"] = projections
+    yield 'start', start_doc
+
+    # Compose descriptor
+    source = 'nxSTXM'
+    frame_data_keys = {'raw': {'source': source,
+                               'dtype': 'number',
+                               'dims': xarray.dims,
+                               # 'coords': [energy, sample_y, sample_x],
+                               'shape': data.shape}}
+    frame_stream_name = 'primary'
+    frame_stream_bundle = run_bundle.compose_descriptor(data_keys=frame_data_keys,
+                                                        name=frame_stream_name,
+                                                        # configuration=_metadata(path)
+                                                        )
+    yield 'descriptor', frame_stream_bundle.descriptor_doc
+
+    # NOTE: Resource document may be meaningful in the future. For transient access it is not useful
+    # # Compose resource
+    # resource = run_bundle.compose_resource(root=Path(path).root, resource_path=path, spec='NCEM_DM', resource_kwargs={})
+    # yield 'resource', resource.resource_doc
+
+    # Compose datum_page
+    # z_indices, t_indices = zip(*itertools.product(z_indices, t_indices))
+    # datum_page_doc = resource.compose_datum_page(datum_kwargs={'index_z': list(z_indices), 'index_t': list(t_indices)})
+    # datum_ids = datum_page_doc['datum_id']
+    # yield 'datum_page', datum_page_doc
+
+    yield 'event', frame_stream_bundle.compose_event(data={'raw': dask_data},
+                                                     timestamps={'raw': time.time()})
+
+    yield 'stop', run_bundle.compose_stop()
+
+
+def ingest_cxi(paths):
+
+    assert len(paths) == 1
+    path = paths[0]
+
+    h5 = h5py.File(path, 'r')
+
+    data = h5['entry_1']['data_1']['data'][()]  # data array
+    translation = h5['entry_1']['data_1']['translation'][()]  # data coords
+    sample_x = translation[:,0]
+    sample_y = translation[:,1]
+    # sample_z = translation[:,2]
+
+    energy = h5['entry_1']['instrument_1']['source_1']['energy'][()] #energy in J
+    wavelength = speed_of_light*h/energy
+    energy_eV = energy/e
+
+    xarray = DataArray(data, dims=['E (eV)', 'y (μm)', 'x (μm)'], coords=[energy_eV, sample_y, sample_x])
+    dask_data = da.from_array(xarray)
+
+    projections = [('NXcxi_ptycho',
+                        {'entry_1/data_1/data': ('primary', 'raw'),
+                         'entry_1/instrument_1/source_1/energy': energy,
+                         'entry_1/data_1/translation': sample_x,
+                         'entry_1/data_1/translation': sample_y}
     )]
 
     # Compose run start
