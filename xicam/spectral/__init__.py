d064a68e6d0ef9d429d9f81af7d16ea8c7332597
ronpandolfi <ronpandolfi@gmail.com>
2020-09-22 12:50:58 -0700

    Add workflow editor (WIP)

--- xicam/spectral/__init__.py
+++ xicam/spectral/__init__.py
@@ -39,71 +39,53 @@ class SpectralPlugin(GUIPlugin):
     name = "Spectral"
 
     def __init__(self):
+        self.current_catalog = None
+
+        # TODO: use catalogs as output of workflows
+        self.current_data = None
+
         self.catalog_viewer = CatalogViewerBlend()
         self.library_viewer = LibraryWidget()
 
         self.treatment_workflow = Workflow()
+        self.treatment_editor = WorkflowEditor(self.treatment_workflow,
+                                               callback_slot=self.show_treatment,
+                                               kwargs_callable=self.treatment_kwargs,
+                                               execute_iterative=True)
 
         self.stages = {
             "Acquire": GUILayout(QWidget()),
             "Library": GUILayout(left=PanelState.Disabled, lefttop=PanelState.Disabled, center=self.library_viewer, right=self.catalog_viewer),
-            "Map": GUILayout(self.catalog_viewer, right=WorkflowEditor(self.treatment_workflow)),
+            "Map": GUILayout(self.catalog_viewer, right=self.treatment_editor),
             "Decomposition": GUILayout(QWidget()),
             "Clustering": GUILayout(QWidget()),
         }
         super(SpectralPlugin, self).__init__()
 
+    def treatment_kwargs(self, workflow):
+
+        # FIXME: Putting this here for now...
+        self.current_data = None
+        return {'data':project_nxSTXM(self.current_catalog)}
+
+    def show_treatment(self, result_set):
+        if self.current_data is None:
+            self.current_data = np.expand_dims(result_set['data'], 0)
+        else:
+            self.current_data = np.stack([*self.current_data, result_set['data']])  # FIXME do this better
+
+        self.catalog_viewer.setImage(self.current_data, reset_crosshair=False)
+
     def appendCatalog(self, run_catalog, **kwargs):
+        self.current_catalog = run_catalog
         try:
-            self.stream_fields = get_all_image_fields(run_catalog)
-            stream_names = get_all_streams(run_catalog)
-
-            msg.showMessage(f"Loading primary image for {run_catalog.name}")
-            # # try and startup with primary catalog and whatever fields it has
-            # if "primary" in self.stream_fields:
-            #     default_stream_name = "primary" if "primary" in stream_names else stream_names[0]
-            # else:
-            #     default_stream_name = list(self.stream_fields.keys())[0]
-
             # Apply nxSTXM projection
             xdata = project_nxSTXM(run_catalog)
 
-            self.catalog_viewer.setImage(xdata)
+            self.catalog_viewer.setImage(xdata, autoRange=True)
 
         except Exception as e:
             msg.logError(e)
             msg.showMessage("Unable to display: ", str(e))
 
 
-### small helper functions
-def get_stream_data_keys(run_catalog, stream):
-    return run_catalog[stream].metadata["descriptors"][0]["data_keys"]
-
-
-def get_all_streams(run_catalog):
-    return list(run_catalog)
-
-
-def get_all_image_fields(run_catalog):
-    # image_fields = []
-    all_streams_image_fields = {}
-    for stream in get_all_streams(run_catalog):
-        stream_fields = get_stream_data_keys(run_catalog, stream)
-        field_names = stream_fields.keys()
-        for field_name in field_names:
-            field_shape = len(stream_fields[field_name]["shape"])
-            if field_shape > 1 and field_shape < 5:
-                # if field contains at least 1 entry that is at least one-dimensional (shape=2)
-                # or 2-dimensional (shape=3) or up to 3-dimensional (shape=4)
-                # then add field e.g. 'fccd_image'
-                if stream in all_streams_image_fields.keys():  # add values to stream dict key
-                    all_streams_image_fields[stream].append(field_name)
-                else:  # if stream does not already exist in dict -> create new entry
-                    all_streams_image_fields[stream] = [field_name]
-            # TODO how to treat non image data fields in streams
-            # else:
-    return all_streams_image_fields
-
-
-# Problem: primary image field does not show up anymore...
-# Problem: primary image field does not show up anymore...
